76f6a2ed02ee56a41e422fdb3c00756001006e0e
Federico Arias <fede98.arias@gmail.com>
2018-11-14 14:25:50 -0300

    Resuelto el conflicto en una migracion

--- db/migrate/20181112225351_create_auctions.rb
+++ db/migrate/20181112225351_create_auctions.rb
@@ -4,13 +4,8 @@ class CreateAuctions < ActiveRecord::Migration[5.1]
       t.ingeger :pujamActual
       t.string :fecha
      # t.users :participantes
-<<<<<<< HEAD
-      t.string :precio
-      t.string :precio
-      t.integer :minimo
-=======
+
       t.integer :precioMinimo
->>>>>>> 2f0c146271d9e858b47797c8a5dce4d43a02b38f
 
      t.timestamps
     end
