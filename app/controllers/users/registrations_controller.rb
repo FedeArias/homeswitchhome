52d38f5f6aa6680e180e4ee72d59c0d817368100
Federico Arias <fede98.arias@gmail.com>
2018-11-29 10:46:03 -0300

    Descomentado de los controladores

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
+   before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
@@ -45,10 +45,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
   # end
 
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
+   #If you have extra params to permit, append them to the sanitizer.
+   def configure_account_update_params
+     devise_parameter_sanitizer.permit(:account_update, keys: [:nombre, :apellido, :DNI, :fechaNacimiento, :codigoTarjeta, :numeroTarjeta, :vencimientoTarjeta])
+   end
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
